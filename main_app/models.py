99792161170bea624cc168901d374874790f8960
Jack <jacool92@gmail.com>
2018-02-01 22:30:36 +0100

    Display in rows

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    pages = models.IntegerField()
+    date = models.DateField()
+    rating = models.IntegerField()
+    comment = models.CharField(max_length=500)
+    img_url = models.ImageField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+
+# Create your views here
 def index(request):
-    name = 'Imię Róży'
-    author = 'Umberto Eco'
-    pages = 500
+    books = Book.objects.all()
+    return render(request, 'index.html', {'books': books})
 
-    return render(request, 'index.html')
+# class Book:
+#     def __init__(self, name, author, pages, description, rating, img_url):
+#         self.name = name
+#         self.author = author
+#         self.pages = pages
+#         self.description = description
+#         self.rating = rating
+#         self.img_url = img_url
+# books = [
+#     Book('Imię Róży', 'Umberto Eco', 500, 'Opis tej przezacnej książki', 10, 'https://www.wydawnictwoliterackie.pl/resources/5/17427-n-a.jpg'),
+#     Book('Krótka Historia Czasu', 'Stephen Hawking', 200, 'Krótko i na temat', 8, 'https://www.wydawnictwoliterackie.pl/resources/5/17427-n-a.jpg'),
+#     Book('Automate Boring Stuff with Python', 'Unknown', 750, 'Yep', 8, 'https://www.wydawnictwoliterackie.pl/resources/5/17427-n-a.jpg')
+# ]
